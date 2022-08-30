969bf8394789c02ecaad261cd89a21db8b04ad4e
Waleson Melo <walesonmelo23@gmail.com>
2022-08-30 12:14:30 -0400

    add funcionalidades do cliente

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import Cadastrar
+from .views import Cadastrar, Alterar, Listar
 
 
 urlpatterns = [
     path('cadastrar/', Cadastrar.as_view(), name='clientes-cadastrar'),
+    path('listar/', Listar.as_view(), name='clientes-listar'),
+    path('alterar/<int:pk>', Alterar.as_view(), name='clientes-alterar'),
 ]
